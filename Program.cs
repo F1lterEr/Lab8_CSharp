3756ae6deccaae839b189473135ad3fec9ced22e
F1lterEr <41ilter.er@gmail.com>
2023-04-06 12:18:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Class<T>
+{
+    List<T> list = new List<T>();
+
+    public void addNumber(params T[] value)
+    {
+        foreach (T a in value)
+        {
+            list.Add(a);
+        }
+    }
+
+    public void deleteNumber(int number)
+    {
+        list.RemoveAt(number);
+    }
+
+    public T getNumber(int number)
+    {
+        return list[number];
+    }
+
+    public int getLenght()
+    {
+        return list.Count;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Class<string> text = new Class<string>();
+        Class<int> number = new Class<int>();
+        Class<double> doubles = new Class<double>();
+
+        text.addNumber("tom", "text", "college");
+        number.addNumber(5, 10, 20);
+        doubles.addNumber(1.35, 0.66, 122.1);
+    }
+}
